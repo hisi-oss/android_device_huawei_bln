65130a6098814d4c64675cdd7d5bc7652b6c82c6
Sebastiano Barezzi <seba@sebaubuntu.dev>
2023-07-18 11:48:10 +0200

    bln: Define camera platform
    Change-Id: Ic71685cc5ee453b60e5562dd0f8eea68d568a09b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,5 +12,8 @@ DEVICE_PATH := device/huawei/bln
 # Display
 TARGET_SCREEN_DENSITY := 460
 
+# Properties
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+
 # Inherit the proprietary files
 include vendor/huawei/bln/BoardConfigVendor.mk
